5daafd590c8be2a93ca750fe2aa20e34bc6a4d1b
kikiisong <55461623+kikiisong@users.noreply.github.com>
2026-09-20 19:55:29 -0400

    Background bulb in progress

--- prototypes/instructions/persona-ui/js/script.js
+++ prototypes/instructions/persona-ui/js/script.js
@@ -20,8 +20,8 @@ function setup() {
 */
 function draw() {
     drawOutline();
-    drawLighterBlueBg();
     drawDarkerBlueBg();
+    drawLighterBlueBg();
     drawBGOverlays();
     drawDay();
     drawMenu();
@@ -35,8 +35,68 @@ function draw() {
  */
 function drawOutline()
 {
+    push();
     noFill();
     stroke("#79d7fd");
     strokeWeight(4);  
     rect(0, 0, 960, 540);
+    pop();
+}
+
+function drawLighterBlueBg()
+{
+    push();
+    noStroke();
+    fill("#2738aa");
+    ellipse(800, 0, 850, 920);
+    pop();
+}
+
+function drawDarkerBlueBg()
+{
+    push();
+    noStroke();
+    fill("#18215f");
+    ellipse(800, 0, 1000, 920);
+    pop();
+}
+
+/**
+ * Some random translucent shapes to add texture
+ */
+function drawBGOverlays()
+{
+    push();
+    noStroke();
+    fill("#18225f82");
+    quad(600, 90, 650, 100, 650, 340, 600, 300);
+    quad(670, 105, 750, 120, 750, 390, 670, 345);
+    quad(770, 125, 920, 105, 920, 420, 770, 400);
+    quad(945, 105, 960, 100, 960, 425, 945, 420);
+    pop();
+}
+
+function drawDay()
+{
+
+}
+
+function drawMenu()
+{
+
+}
+
+function drawRedDebris()
+{
+
+}
+
+function drawTitle()
+{
+
+}
+
+function drawProtagonist()
+{
+
 }
