1171b8a7dd4fdff115e70288fdec62a191ada693
kikiisong <55461623+kikiisong@users.noreply.github.com>
2026-09-22 11:08:12 -0400

    draw a blueberry swiss roll

--- prototypes/instructions/blueberry-swiss-roll/js/script.js
+++ prototypes/instructions/blueberry-swiss-roll/js/script.js
@@ -8,16 +8,44 @@
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Setting up the canvas
 */
 function setup() {
-
+    createCanvas(420, 420);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draw the parts
 */
 function draw() {
+    // color background
+    background("#FF9CB5");
 
+    push();
+    // draw the roll
+    noStroke();
+    for(let x = 0; x<=5; x++)
+    {
+        if( x%2 ===0)
+        {
+            fill("#9c66d9");       
+        }else
+        {
+            fill("#fefcff");
+        }
+        ellipse(210,210, 50*(6-x), 50*(6-x));
+    }
+    
+    // draw leaf & blueberries
+    rotate(radians(-10));
+    fill("#036e10"); 
+    ellipse(245,100, 20, 50);
+    rotate(radians(10));
+    fill("#4b3663"); 
+    ellipse(250,55, 30, 30);
+    fill("#4b3663"); 
+    ellipse(270,65, 25, 25);
+
+    pop();
 }
