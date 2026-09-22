c7b56a916c3f0e83c28a7016b6be13932dd6c5a5
kikiisong <55461623+kikiisong@users.noreply.github.com>
2026-09-22 15:20:05 -0400

    Changing color for the sky & Mr. Furious + Added bird

--- topics/variables-challenge/js/script.js
+++ topics/variables-challenge/js/script.js
@@ -1,24 +1,86 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Mr. Furious
+ * Pippin Barr
+ *
+ * A guy who becomes visibly furious!
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
+// Our friend Mr. Furious
+let mrFurious = {
+  // Position and size
+  x: 200,
+  y: 200,
+  size: 100,
+  // Colour
+  fill: {
+    r: 255,
+    g: 225,
+    b: 225
+  }
 
+};
+
+let sky = {
+    fill: {
+    r: 160,
+    g: 180,
+    b: 200,
+  }
+};
+
+let bird = {
+    fill: 
+    {
+        r: 0,
+        g: 204,
+        b: 0 
+    },
+    pos_x:
+    {
+        first: 20,
+        second: 50,
+        third:7
+    },
+    pos_y:
+    {
+        first: 20,
+        second: 30,
+        third: 25
+    },
+    velocity: 2
+};
+
+/**
+ * Create the canvas
+ */
+function setup() {
+  createCanvas(400, 400);
 }
 
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Draw (and update) Mr. Furious
+ */
 function draw() {
+background(
+  sky.fill.r -= 1,
+  sky.fill.g -= 1,
+  sky.fill.b -= 1)
+  
+  // Draw Mr. Furious as a coloured circle
+  push();
+  noStroke();
+  mrFurious.fill.g -= 1;
+  mrFurious.fill.b -= 1;
+  fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
+  ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+  pop();
 
+  //Draw bird
+  push();
+  noStroke();
+  fill(bird.fill.r,bird.fill.g,bird.fill.b);
+  triangle(bird.pos_x.first+=bird.velocity, bird.pos_y.first, bird.pos_x.second +=bird.velocity, bird.pos_y.second, bird.pos_x.third+=bird.velocity, bird.pos_y.third);
+  pop();
 }
